cd852a37b9059503db919e38631eabfa9b48d4e2
Chris Montes <cmmontes2@gmail.com>
2015-11-25 19:05:00 -0800

    js

--- assignment/js/jquery.js
+++ assignment/js/jquery.js
@@ -1,75 +1,35 @@
-/* Hide function */
+$(document).ready(function() {
 
-$('.hidden').hide();
+  $('.readmore').click(showMore);
+  //show more
+  function showMore(){
+    event.preventDefault();
+    console.log('read more button clicked');
+    //hide read more button
+    $('.readmore').hide();
+    //show hidden text
+    $('#show-this-on-click').slideDown();
+    //show read less 
+    $('.readless').show();
+  };
 
+  $('.readless').click(showLess);
+  //show less of blog post
+  function showLess() {
+    event.preventDefault();
+    //Hide read less button
+    $('.readless').hide();
+    //show less text
+    $('#show-this-on-click').slideUp(function() {
+    //show read more button
+    $('.readmore').show();
+    });
+  };
 
-/* prevent social links */
+  $('.learnmore').click(learnMore);
+  //Learn more about Relaxr
+  function learnMore() {
+    $('#learnmoretext').show();
+  }
 
-$('#social').click(function(event) {
-	event.preventDefault();
 });
-
-
-/* aside js */
-
-$('#sidep').hide();
-
-$(document).ready(function(){
-	$("button").click(function(){
-		$("#sidep").slideToggle();
-
-	});
-});
-
-
-/*
-
-function question8(){
- 	$(".city").attr("src","http://lorempixel.com/g/500/400/food");
- }
-
- 	$("#button3").click(question8);
-
-
-
-function question2(){
-		$("h2").text("Chris");
-	 }
-
-	 $("#button1").click(question2);
-
-
-
-*/
-
-
-/*  Blog Post One */
-
-$(document).ready(function () {
-   $('.posttext').hide();
-
-   $('.read_more').click(function (event) {
-       event.preventDefault();
-       $(this).parent().find('.posttext').slideToggle('slow');
-       $(this).text($(this).text() == 'Read less >' ? 'Read more >' : 'Read less >');
-   });
-});
-
-
-/*  Blog Post Two */
-
-$(document).ready(function () {
-   $('.post_two').hide();
-
-   $('#more_read2').click(function (event) {
-       event.preventDefault();
-       $(this).parent().find('.post_two').slideToggle('slow');
-       $(this).text($(this).text() == 'Read less >' ? 'Read more >' : 'Read less >');
-   });
-});
-
-
-
-
-
-
